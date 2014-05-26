713dcdfc614581660319e9beff8478ff554aca50
smiletic1 <smiletic1@etf.unsa.ba>
2014-05-26 09:07:17 +0200

    mainframe

---
+++ data-vision/src/main/java/etf/si/projekat/data_vision/GlavniFrame.java
@@ -0,0 +1,95 @@
+package etf.si.projekat.data_vision;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.border.CompoundBorder;
+import javax.swing.BoxLayout;
+
+public class GlavniFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GlavniFrame frame = new GlavniFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GlavniFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 550, 406);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setToolTipText("");
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Data Vision");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+		
+				
+			}
+		});
+		mnNewMenu.add(mntmExit);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnNewMenu.add(mntmAbout);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBorder(new CompoundBorder());
+		contentPane.add(tabbedPane);
+		
+		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Data Vision", null, tabbedPane_2, null);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("One graph view", null, tabbedPane_1, null);
+		
+		JTabbedPane tabbedPane_3 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Two graph view", null, tabbedPane_3, null);
+		
+		JTabbedPane tabbedPane_4 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Three graph view", null, tabbedPane_4, null);
+		
+		JTabbedPane tabbedPane_5 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Table view", null, tabbedPane_5, null);
+		
+		JTabbedPane tabbedPane_6 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Consumption", null, tabbedPane_6, null);
+		
+		
+	}
+
+}
