5be98dacc179a4bcf09cdb92d49a6b89ccfa42d4
mkiselica1 <mkiselica1@etf.unsa.ba>
2014-05-26 04:23:53 +0200

    hibernate user

--- data-vision/src/main/java/ba/unsa/etf/si/hibernate_klase/HibernateUser.java
+++ data-vision/src/main/java/ba/unsa/etf/si/hibernate_klase/HibernateUser.java
@@ -1,5 +1,18 @@
 package ba.unsa.etf.si.hibernate_klase;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import etf.si.projekat.util.HibernateUtil;
+
+
+
 public class HibernateUser {
+	
+	Session session;
+    Transaction t;
+    
+    
+	
 
 }
