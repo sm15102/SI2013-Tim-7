a7a019890d4e9ded3998e9c8ee929cfe107e6be0
smiletic1 <smiletic1@etf.unsa.ba>
2014-05-31 22:44:27 +0200

    addedMoreData in devicetype

--- Database/devicetype.sql
+++ Database/devicetype.sql
@@ -2,8 +2,14 @@
 -- Query: select * from devicetype
 LIMIT 0, 1000
 
--- Date: 2014-05-31 21:20
+-- Date: 2014-05-31 22:44
 */
 INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (1,'window');
 INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (2,'light');
 INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (3,'temperature');
+INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (4,'heat');
+INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (5,'light1');
+INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (6,'light2');
+INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (7,'water');
+INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (8,'smoke');
+INSERT INTO `devicetype` (`TYPE_ID`,`TYPE`) VALUES (9,'heat1');
