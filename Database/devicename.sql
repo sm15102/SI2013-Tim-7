f6f3d18b1ec338c81a00cddb0a2475dd3e86b264
smiletic1 <smiletic1@etf.unsa.ba>
2014-05-31 22:55:49 +0200

    devicename

--- Database/devicename.sql
+++ Database/devicename.sql
@@ -1,9 +1,9 @@
-/*
--- Query: select * from devicename
-LIMIT 0, 1000
-
--- Date: 2014-05-31 21:21
-*/
-INSERT INTO `devicename` (`DEVICENAME_ID`,`NAME`) VALUES (1,'Light_Blob');
-INSERT INTO `devicename` (`DEVICENAME_ID`,`NAME`) VALUES (2,'Window');
-INSERT INTO `devicename` (`DEVICENAME_ID`,`NAME`) VALUES (3,'Temperature');
+DEVICENAME_ID,NAME
+1,Light_Blob
+2,Window
+3,Temperature
+11,heat
+12,water
+13,smoke
+14,air
+15,wind
