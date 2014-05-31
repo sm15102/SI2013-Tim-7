93754e302d1c239b2a7ae868d1e9525ab2b3e75a
smiletic1 <smiletic1@etf.unsa.ba>
2014-05-31 21:39:40 +0200

    Copyright, aboutUS

--- data-vision/src/main/java/etf/si/projekat/data_vision/About.java
+++ data-vision/src/main/java/etf/si/projekat/data_vision/About.java
@@ -49,57 +49,57 @@ public class About extends JFrame {
 	public About() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 285, 300);
+		setBounds(100, 100, 161, 283);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblTeam = new JLabel("Comming soon");
+		JLabel lblTeam = new JLabel("Made by");
 		lblTeam.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblTeam.setBounds(10, 10, 108, 28);
 		contentPane.add(lblTeam);
 		
-		JLabel lblTeam_1 = new JLabel("maybe later");
+		JLabel lblTeam_1 = new JLabel("TEAM7");
 		lblTeam_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblTeam_1.setBounds(10, 35, 93, 14);
 		contentPane.add(lblTeam_1);
 		
-		JLabel lblDevelopers = new JLabel("Starring");
+		JLabel lblDevelopers = new JLabel("Developers");
 		lblDevelopers.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblDevelopers.setBounds(10, 75, 156, 14);
 		contentPane.add(lblDevelopers);
 		
 		JLabel lblMelikaKiselica = new JLabel("Melika Kiselica");
-		lblMelikaKiselica.setBounds(141, 91, 103, 14);
+		lblMelikaKiselica.setBounds(10, 100, 103, 14);
 		contentPane.add(lblMelikaKiselica);
 		
 		JLabel lblEnesMujic = new JLabel("Enes Mujic");
-		lblEnesMujic.setBounds(10, 100, 103, 14);
+		lblEnesMujic.setBounds(10, 172, 103, 14);
 		contentPane.add(lblEnesMujic);
 		
 		JLabel lblSnjezanaMiletic = new JLabel("Snjezana Miletic");
-		lblSnjezanaMiletic.setBounds(63, 116, 103, 14);
+		lblSnjezanaMiletic.setBounds(10, 131, 103, 14);
 		contentPane.add(lblSnjezanaMiletic);
 		
 		JLabel lblSalemMaglic = new JLabel("Salem Maglic");
-		lblSalemMaglic.setBounds(155, 222, 103, 14);
+		lblSalemMaglic.setBounds(10, 156, 103, 20);
 		contentPane.add(lblSalemMaglic);
 		
 		JLabel lblAdnanOmanovic = new JLabel("Adnan Omanovic");
-		lblAdnanOmanovic.setBounds(0, 141, 103, 14);
+		lblAdnanOmanovic.setBounds(10, 141, 103, 20);
 		contentPane.add(lblAdnanOmanovic);
 		
 		JLabel lblNadinaKunalic = new JLabel("Nadina Kunalic");
-		lblNadinaKunalic.setBounds(136, 141, 108, 14);
+		lblNadinaKunalic.setBounds(10, 116, 108, 14);
 		contentPane.add(lblNadinaKunalic);
 		
 		JLabel lblRijadMuhic = new JLabel("Rijad Muhic");
-		lblRijadMuhic.setBounds(27, 174, 76, 14);
+		lblRijadMuhic.setBounds(10, 187, 76, 14);
 		contentPane.add(lblRijadMuhic);
 		
-		JLabel lblcopyrightByTeam = new JLabel("Made in China");
-		lblcopyrightByTeam.setBounds(66, 247, 192, 14);
+		JLabel lblcopyrightByTeam = new JLabel(" © Copyright Team7");
+		lblcopyrightByTeam.setBounds(10, 227, 192, 14);
 		contentPane.add(lblcopyrightByTeam);
 		
 		JLabel label = new JLabel("");
