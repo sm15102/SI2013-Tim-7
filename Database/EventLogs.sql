3f0b806895f3301b56297087239bd4780f2bda8f
aomanovic <ao15155@etf.unsa.ba>
2014-06-02 23:51:16 +0200

    Update EventLogs.sql
    Popravljen sql querry za popunjavanje tabele eventlogs

--- Database/EventLogs.sql
+++ Database/EventLogs.sql
@@ -1,21 +1,20 @@
-EVENTLOGS_ID,DEVICE_NAME,DEVICE_TYPE,EVENT_MESSAGE,TIMESTAMP,ACTIVITY_LOGS,value
-1,Light_Blob,light,poruka,"2014-01-01 12:12:12",NULL,515
-2,Light_Blob,light,poruka,"2014-01-01 12:12:12",NULL,313
-3,Window,window,"poruka o stanju","2014-01-01 12:12:12",NULL,100
-4,Window,window,"poruka o stanju","2014-01-01 12:12:12",NULL,180
-5,Light_Blob,light,"poruka o stanju","2014-01-01 12:12:12",NULL,45
-6,Window,window,"poruka o stanju","2014-01-01 12:12:12",NULL,69
-7,Window,window,"poruka o stanju","2014-01-01 12:12:12",NULL,89
-8,Window,window,"poruka o stanju","2014-01-01 12:12:12",NULL,54
-9,Window,window,"poruka o stanju","2014-01-01 12:12:12",1,450
-10,Window,window,"poruka o stanju","2014-01-01 12:12:12",1,36
-11,Window,window,"poruka o stanju","2014-01-01 12:12:12",1,14
-12,Window,window,"poruka o stanju","2014-01-01 12:12:12",1,87
-13,Window,window,"poruka o stanju","2014-01-01 12:12:12",1,54
-14,Light_Blob,light,"poruka o stanju","2014-01-01 12:12:12",1,99
-15,Light_Blob,light,"poruka o stanju","2014-01-01 12:12:12",1,888
-16,Temperature,temperature,"poruka o stanju","2014-05-01 12:12:12",1,55
-17,Temperature,temperature,"poruka o stanju","2014-05-10 12:12:12",1,4
-18,Temperature,temperature,"poruka o stanju","2014-05-05 12:12:12",1,2
-19,Temperature,temperature,"poruka o stanju","2014-05-30 12:12:12",1,10
-20,Temperature,temperature,"poruka o stanju","2014-05-31 12:12:12",1,12
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (1,'Light_Blob','light','poruka','2014-01-01 12:12:12',1,515);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (2,'Light_Blob','light','poruka','2014-01-01 12:12:12',1,313);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (3,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,100);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (4,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,180);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (5,'Light_Blob','light','poruka o stanju','2014-01-01 12:12:12',1,45);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (6,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,69);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (7,'Radiator','heat','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (8,'Radiator','heat','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (9,'Radiator','heat','poruka o stanju','2014-01-01 12:12:12',1,450);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (10,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (11,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (12,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (13,'Window','window','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (14,'Light_Blob','light','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (15,'Light_Blob','light','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (16,'Temperature','temperature','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (17,'Temperature','temperature','poruka o stanju','2014-01-01 12:12:12',1,0);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (18,'Temperature','temperature','poruka o stanju','2014-01-01 12:12:12',1,45);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (19,'Temperature','temperature','poruka o stanju','2014-04-30 12:12:12',1,10);
+INSERT INTO `eventlogs` (`EVENTLOGS_ID`,`DEVICE_NAME`,`DEVICE_TYPE`,`EVENT_MESSAGE`,`TIMESTAMP`,`ACTIVITY_LOGS`,`value`) VALUES (20,'Temperature','temperature','poruka o stanju','2014-04-30 12:12:12',1,12);
