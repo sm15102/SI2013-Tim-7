2e3546e212680d0c2dd5294e9d1c67cd5e46ef8c
aomanovic <ao15155@etf.unsa.ba>
2014-06-10 19:49:23 +0200

    Update SensorChoosingPanel.java

--- data-vision/src/main/java/etf/si/projekat/data_vision/SensorChoosingPanel.java
+++ data-vision/src/main/java/etf/si/projekat/data_vision/SensorChoosingPanel.java
@@ -26,88 +26,88 @@ public class SensorChoosingPanel extends JPanel {
 		lblSensorType.setBounds(30, 61, 95, 14);
 		add(lblSensorType);
 		
-		JLabel lblSensorType_1 = new JLabel("Sensor type 3");
-		lblSensorType_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_1.setBounds(30, 87, 95, 14);
-		add(lblSensorType_1);
+		JLabel lblSensorType1 = new JLabel("Sensor type 3");
+		lblSensorType1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType1.setBounds(30, 87, 95, 14);
+		add(lblSensorType1);
 		
-		JLabel lblSensorType_2 = new JLabel("Sensor type 4");
-		lblSensorType_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_2.setBounds(30, 113, 95, 14);
-		add(lblSensorType_2);
+		JLabel lblSensorType2 = new JLabel("Sensor type 4");
+		lblSensorType2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType2.setBounds(30, 113, 95, 14);
+		add(lblSensorType2);
 		
-		JLabel lblSensorType_3 = new JLabel("Sensor type 5");
-		lblSensorType_3.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_3.setBounds(30, 141, 95, 14);
-		add(lblSensorType_3);
+		JLabel lblSensorType3 = new JLabel("Sensor type 5");
+		lblSensorType3.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType3.setBounds(30, 141, 95, 14);
+		add(lblSensorType3);
 		
-		JLabel lblSensorType_4 = new JLabel("Sensor type 6");
-		lblSensorType_4.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_4.setBounds(30, 165, 95, 14);
-		add(lblSensorType_4);
+		JLabel lblSensorType4 = new JLabel("Sensor type 6");
+		lblSensorType4.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType4.setBounds(30, 165, 95, 14);
+		add(lblSensorType4);
 		
-		JLabel lblSensorType_5 = new JLabel("Sensor type 7");
-		lblSensorType_5.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_5.setBounds(30, 192, 95, 14);
-		add(lblSensorType_5);
+		JLabel lblSensorType5 = new JLabel("Sensor type 7");
+		lblSensorType5.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType5.setBounds(30, 192, 95, 14);
+		add(lblSensorType5);
 		
-		JLabel lblSensorType_6 = new JLabel("Sensor type 8");
-		lblSensorType_6.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_6.setBounds(30, 216, 95, 14);
-		add(lblSensorType_6);
+		JLabel lblSensorType6 = new JLabel("Sensor type 8");
+		lblSensorType6.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType6.setBounds(30, 216, 95, 14);
+		add(lblSensorType6);
 		
-		JLabel lblSensorType_7 = new JLabel("Sensor type 9");
-		lblSensorType_7.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_7.setBounds(30, 241, 95, 14);
+		JLabel lblSensorType7 = new JLabel("Sensor type 9");
+		lblSensorType7.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType7.setBounds(30, 241, 95, 14);
 		
 		Choice choice = new Choice();
 		choice.setBounds(158, 30, 138, 20);
 		add(choice);
 		
-		Choice choice_1 = new Choice();
-		choice_1.setBounds(158, 55, 138, 20);
-		add(choice_1);
+		Choice choice1 = new Choice();
+		choice1.setBounds(158, 55, 138, 20);
+		add(choice1);
 		
-		Choice choice_2 = new Choice();
-		choice_2.setBounds(158, 81, 138, 20);
-		add(choice_2);
+		Choice choice2 = new Choice();
+		choice2.setBounds(158, 81, 138, 20);
+		add(choice2);
 		
-		Choice choice_3 = new Choice();
-		choice_3.setBounds(158, 107, 138, 20);
-		add(choice_3);
+		Choice choice3 = new Choice();
+		choice3.setBounds(158, 107, 138, 20);
+		add(choice3);
 		
-		Choice choice_4 = new Choice();
-		choice_4.setBounds(158, 135, 138, 20);
-		add(choice_4);
+		Choice choice4 = new Choice();
+		choice4.setBounds(158, 135, 138, 20);
+		add(choice4);
 		
-		Choice choice_5 = new Choice();
-		choice_5.setBounds(158, 159, 138, 20);
-		add(choice_5);
+		Choice choice5 = new Choice();
+		choice5.setBounds(158, 159, 138, 20);
+		add(choice5);
 		
-		Choice choice_6 = new Choice();
-		choice_6.setBounds(158, 186, 138, 20);
-		add(choice_6);
+		Choice choice6 = new Choice();
+		choice6.setBounds(158, 186, 138, 20);
+		add(choice6);
 		
-		Choice choice_7 = new Choice();
-		choice_7.setBounds(158, 210, 138, 20);
-		add(choice_7);
+		Choice choice7 = new Choice();
+		choice7.setBounds(158, 210, 138, 20);
+		add(choice7);
 		
-		Choice choice_8 = new Choice();
-		choice_8.setBounds(158, 235, 138, 20);
-		add(choice_8);
+		Choice choice8 = new Choice();
+		choice8.setBounds(158, 235, 138, 20);
+		add(choice8);
 		
 		JSeparator separator = new JSeparator();
 		separator.setBounds(0, 21, 337, 2);
 		add(separator);
 		
-		JLabel lblSensorType_8 = new JLabel("Sensor type ");
-		lblSensorType_8.setBounds(54, 0, 83, 25);
-		add(lblSensorType_8);
+		JLabel lblSensorType8 = new JLabel("Sensor type ");
+		lblSensorType8.setBounds(54, 0, 83, 25);
+		add(lblSensorType8);
 		
-		JLabel lblSensorType_9 = new JLabel("Sensor type 9");
-		lblSensorType_9.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSensorType_9.setBounds(30, 241, 95, 14);
-		add(lblSensorType_9);
+		JLabel lblSensorType9 = new JLabel("Sensor type 9");
+		lblSensorType9.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSensorType9.setBounds(30, 241, 95, 14);
+		add(lblSensorType9);
 
 	}
 
